3b510b914b5d53a07498b66a1ab1c9b958f21e2a
TShabatura <52471913+TShabatura@users.noreply.github.com>
2019-11-24 18:53:11 +0200

    Add files via upload

---
+++ Student/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            using (StudentContext db = new StudentContext())
+            {
+                //Додавання даних в Базу Даних 
+                Console.WriteLine("Введіть ім'я студента");
+                Console.WriteLine("Щоб закінчити, введіть end");
+                string name = Console.ReadLine();
+                while (name != "end")
+                {
+                    Student student = new Student { Name = name };
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    name = Console.ReadLine();
+                }
+                Console.WriteLine("Список успішно збережений!");
+
+
+                var students = db.Students;
+                Console.WriteLine("Список студентів:");
+                foreach (Student u in students)
+                {
+                    Console.WriteLine("{0}.{1}", u.Id, u.Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
